7982c6b69676f67e8cc10dca97e5881114f10209
Sam Morrow <smorrow@oberlin.edu>
2017-06-05 16:58:48 -0400

    models finished

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,3 +3,20 @@ const Order = require('./order');
 const Product = require('./product');
 const Review = require('./review');
 const ProductOrder = require('./product_order');
+
+Order.belongsTo(User);
+Review.belongsTo(User);
+
+User.hasMany(Order);
+User.hasMany(Review);
+
+Product.belongsToMany(Order, {through: ProductOrder});
+Order.belongsToMany(Product, {through: ProductOrder});
+
+module.exports = {
+  User,
+  Order,
+  Product,
+  Review,
+  ProductOrder
+}
