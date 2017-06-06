373e6b8795f47b89a4d44da456271589d2a781a4
Jason Oscar <jhoscar@gmail.com>
2017-06-06 11:42:35 -0400

    removing total cost from order model.

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -6,12 +6,6 @@ module.exports = db.define('order', {
   purchase_date: {
     type: Sequelize.DATE,
   },
-  total_cost: {
-    type: Sequelize.INTEGER,
-    get: function(){
-      return this.getDataValue('total_cost') / 100
-    }
-  },
   status: {
     type: Sequelize.ENUM('created', 'processing', 'cancelled', 'completed')
   }
