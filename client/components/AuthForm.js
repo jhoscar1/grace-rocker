01f3c55277e163148c8de0f1025e409edc8d61c0
Howard Jian <hcwjian@gmail.com>
2017-06-12 18:30:31 -0400

    added pw validation for 3 characters

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,57 +1,76 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const AuthForm = props => {
+// const AuthForm = props => {
+class AuthForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pwInvalid: true
+    }
+    this.handlePwChange = this.handlePwChange.bind(this);
+  }
 
-  const { name, displayName, handleSubmit, error } = props;
+  handlePwChange(evt) {
+    if(evt.target.value.length >= 3) {
+      this.setState({pwInvalid: false})
+    } else {
+      this.setState({pwInvalid: true})
+    }
+  }
 
-  const signUpData = (
-    <div>
+  render() {
+
+    const { name, displayName, handleSubmit, error } = this.props;
+
+    const signUpData = (
       <div>
-        <label htmlFor="shippingAddress"><small>Shipping Address</small></label>
-        <input name="shippingAddress" type="text" />
-      </div>
-      <div>
-        <label htmlFor="userName"><small>Name</small></label>
-        <input name="userName" type="text" />
-      </div>
-    </div>
-  );
-
-  const updateData = (
-    <div>
-
-      <div>
-        <label htmlFor="isAdmin"><small>Admin Status</small></label>
-        <select name="isAdmin">
-          <option> true </option>
-          <option> false </option>
-        </select>
-      </div>
-    </div>
-  )
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit} name={name}>
         <div>
-          <label htmlFor="email"><small>Email</small></label>
-          <input name="email" type="text" />
+          <label htmlFor="shippingAddress"><small>Shipping Address</small></label>
+          <input name="shippingAddress" type="text" />
         </div>
         <div>
-          <label htmlFor="password"><small>Password</small></label>
-          <input name="password" type="password" />
+          <label htmlFor="userName"><small>Name</small></label>
+          <input name="userName" type="text" />
         </div>
-        {displayName === 'Sign Up' || displayName === 'Update' ? signUpData : null}
-        {displayName === 'Update' ? updateData : null}
+      </div>
+    );
+
+    const updateData = (
+      <div>
+
         <div>
-          <button type="submit">{ displayName }</button>
+          <label htmlFor="isAdmin"><small>Admin Status</small></label>
+          <select name="isAdmin">
+            <option> true </option>
+            <option> false </option>
+          </select>
         </div>
-        { error &&  <div> { error.response.data } </div> }
-      </form>
-      <a href="/auth/google">{ displayName } with Google</a>
-    </div>
-  );
+      </div>
+    )
+
+    return (
+      <div>
+        <form onSubmit={handleSubmit} name={name}>
+          <div>
+            <label htmlFor="email"><small>Email</small></label>
+            <input name="email" type="text" />
+          </div>
+          <div>
+            <label htmlFor="password"><small>Password</small></label>
+            <input onChange={this.handlePwChange} name="password" type="password" />
+          </div>
+          {displayName === 'Sign Up' || displayName === 'Update' ? signUpData : null}
+          {displayName === 'Update' ? updateData : null}
+          <div>
+            <button disabled={this.state.pwInvalid} type="submit">{ displayName }</button>
+          </div>
+          { error &&  <div> { error.response.data } </div> }
+        </form>
+        <a href="/auth/google">{ displayName } with Google</a>
+      </div>
+    );
+  }
 };
 
 export default AuthForm;
