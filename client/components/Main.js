fa1680a7b78f546f161760a2ea579ff7772e95fa
Sam Morrow <smorrow@oberlin.edu>
2017-06-13 21:38:36 -0400

    its a dropdown now

--- client/components/Main.js
+++ client/components/Main.js
@@ -7,35 +7,53 @@ import axios from 'axios';
 
 // Component //
 
-const Main = props => {
+class Main extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      showDropdown: false
+    }
+    this.exposeDropDown = this.exposeDropDown.bind(this);
+  }
 
-  const { children, handleClick, loggedIn, isAdmin, cart } = props;
+  exposeDropDown(){
+    console.log(this.state.showDropdown)
+    this.setState({
+      showDropdown: !this.state.showDropdown
+    })
+  }
 
-  return (
-    <div>
-      <nav className="row flex relative">
-        <h2 className="left bottom"> GRACE ROCKER </h2>
-        <Link className="left" to={'/home'}><img className="logo" src="/assets/gracerocker.png"/></Link>
-      <div className="col-6-sm col-4-md iconContainer navbar-right relative">
-        { loggedIn ?
-          <div>
-            <Link style={{ textDecoration: 'none'}} className="noDecoration right navGlyph glyphicon glyphicon-home" id="home" title="Main Page" to="/home"></Link>
-            <a style={{ textDecoration: 'none'}} className="right navGlyph glyphicon glyphicon-log-out" id="signout" title="Log out" href="#" onClick={handleClick}></a>
-            { isAdmin ? <Link style={{ textDecoration: 'none'}} className="right" to="/admin">Admin</Link> : null }
-          </div> : <div className="nav-div right">
-            <Link style={{ textDecoration: 'none'}} to="/login">Login</Link>
-            <Link style={{ textDecoration: 'none'}} to="/signup">Sign Up</Link>
-          </div>
-        }
+  render(){
+    const { children, handleClick, loggedIn, isAdmin, cart, userId } = this.props;
+    return (
+      <div>
+        <nav className="row flex relative">
+          <h2 className="left bottom"> GRACE ROCKER </h2>
+          <Link className="left" to={'/home'}><img className="logo" src="/assets/gracerocker.png"/></Link>
+        <div className="col-6-sm col-4-md iconContainer navbar-right relative">
+          { loggedIn ?
+            <div>
+              <button className="inline navGlyph glyphicon glyphicon-user" id="accountPage"  onClick={this.exposeDropDown} />
+              { this.state.showDropdown ? <div className="dropdownContent">
+                <a><Link style={{ textDecoration: 'none'}} className="" id="home" title="Main Page" to="/home">Home</Link></a>
+                <a style={{ textDecoration: 'none'}} className="" id="signout" title="Log out" href="#" onClick={handleClick}>Logout</a>
+                { isAdmin ? <a><Link style={{ textDecoration: 'none'}} className="right" to="/admin">Admin</Link></a> : null }
+                <Link style={{ textDecoration: 'none'}} title="Your Userpage" className="" to={`/users/${userId}`}>Your Account</Link>
+              </div> : null }
+            </div> : <div className="nav-div right">
+              <Link style={{ textDecoration: 'none'}} to="/login">Login</Link>
+              <Link style={{ textDecoration: 'none'}} to="/signup">Sign Up</Link>
+            </div>
+          }
 
-            <Link title="Your Cart" to="/cart"><span id="shoppingCart" className="inline navGlyph glyphicon glyphicon-shopping-cart"><p className="left shoppingCartCount">{cart.products ? cart.products.length : 0 }</p></span></Link>
-            <Link title="Your Userpage" to={`/users/${userId}`}>My Account</Link>
-          </div>
-      </nav>
-      { children }
-    </div>
-  );
-};
+              <Link title="Your Cart" to="/cart"><span id="shoppingCart" className="inline navGlyph glyphicon glyphicon-shopping-cart"><p className="left shoppingCartCount">{cart.products ? cart.products.length : 0 }</p></span></Link>
+            </div>
+        </nav>
+        { children }
+      </div>
+    );
+  };
+}
 
 Main.propTypes = {
   children: PropTypes.object,
@@ -48,7 +66,7 @@ Main.propTypes = {
 const mapState = ({ userReducer, cartReducer}) => ({
   loggedIn: !!userReducer.user.id,
   isAdmin: userReducer.user.isAdmin,
-  cart: cartReducer.cart
+  cart: cartReducer.cart,
   userId: userReducer.user.id
 });
 
