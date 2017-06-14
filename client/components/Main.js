c30be37e7937144e20bbc9d86fc21f409420004d
Sam Morrow <smorrow@oberlin.edu>
2017-06-13 20:42:38 -0400

    navbar done?

--- client/components/Main.js
+++ client/components/Main.js
@@ -13,18 +13,18 @@ const Main = props => {
 
   return (
     <div>
-      <nav className="row flex relative navbarContainer">
+      <nav className="row flex relative">
         <h2 className="left bottom"> GRACE ROCKER </h2>
         <Link className="left" to={'/home'}><img className="logo" src="/assets/gracerocker.png"/></Link>
-      <div className="col-6-sm col-4-md iconContainer relative">
+      <div className="col-6-sm col-4-md iconContainer navbar-right relative">
         { loggedIn ?
           <div>
-            <Link className="right navGlyph glyphicon glyphicon-home" id="home" title="Main Page" to="/home"></Link>
-            <a className="right navGlyph glyphicon glyphicon-log-out" id="signout" title="Log out" href="#" onClick={handleClick}></a>
-            { isAdmin ? <Link className="right" to="/admin">Admin</Link> : null }
+            <Link style={{ textDecoration: 'none'}} className="noDecoration right navGlyph glyphicon glyphicon-home" id="home" title="Main Page" to="/home"></Link>
+            <a style={{ textDecoration: 'none'}} className="right navGlyph glyphicon glyphicon-log-out" id="signout" title="Log out" href="#" onClick={handleClick}></a>
+            { isAdmin ? <Link style={{ textDecoration: 'none'}} className="right" to="/admin">Admin</Link> : null }
           </div> : <div className="nav-div right">
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
+            <Link style={{ textDecoration: 'none'}} to="/login">Login</Link>
+            <Link style={{ textDecoration: 'none'}} to="/signup">Sign Up</Link>
           </div>
         }
 
